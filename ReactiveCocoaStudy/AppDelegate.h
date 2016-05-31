f8be27969ba18c73caf8c2d63429711d51afa341
xiaoshi <sjl_leaf@163.com>
2016-05-31 15:32:45 +0800

    Initial Commit

---
+++ ReactiveCocoaStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReactiveCocoaStudy
+//
+//  Created by xiaoshi on 16/5/31.
+//  Copyright © 2016年 https://github.com/shijinliang, http://blog.csdn.net/sjl_leaf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReactiveCocoaStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReactiveCocoaStudy
+//
+//  Created by xiaoshi on 16/5/31.
+//  Copyright © 2016年 https://github.com/shijinliang, http://blog.csdn.net/sjl_leaf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
